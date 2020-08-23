d617d4f1237bddb82acd03d80136fac1e57d06bc
Zhivotenko Aleksandr <zhivotenko.a.s@gmail.com>
2020-08-23 19:13:18 +0300

    добавляет дз5

--- js/notDone/factorial.js
+++ js/notDone/factorial.js
@@ -1,10 +1,30 @@
-const hotel = {
-    name: 'Resort Hotel',
-    stars: 5,
-    capacity: 100,
-};
+class Hero {
+    constructor({ name = "hero", xp = 0 } = {}) {
+        this.name = name;
+        this.xp = xp;
 
-const { name, ...rest } = hotel;
+    }
 
-console.log(name); // "Resort Hotel"
-console.log(rest); // {stars: 5, capacity: 100}
+    gainXp(amount) {
+        this.xp += amount;
+    }
+}
+
+class Warrior extends Hero {
+    constructor(name, xp, weapon) {
+        super(name, xp);
+        this.weapon = weapon;
+    }
+
+    gainXp() {}
+}
+// class Mage {
+//     constructor() {
+
+//         this.spells = [];
+//     }
+
+//     gainXp() {}
+// }
+const man = new Warrior("Man", 1000, "меч");
+console.log(man);
