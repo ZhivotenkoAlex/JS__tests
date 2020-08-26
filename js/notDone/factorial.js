add3e73e954bedb17746d64ae6e66e6d790972b6
Zhivotenko Aleksandr <zhivotenko.a.s@gmail.com>
2020-08-27 01:56:35 +0300

    вносит первые 3 задачи дз№6

--- js/notDone/factorial.js
+++ js/notDone/factorial.js
@@ -1,30 +1,8 @@
-class Hero {
-    constructor({ name = "hero", xp = 0 } = {}) {
-        this.name = name;
-        this.xp = xp;
-
-    }
-
-    gainXp(amount) {
-        this.xp += amount;
-    }
-}
-
-class Warrior extends Hero {
-    constructor(name, xp, weapon) {
-        super(name, xp);
-        this.weapon = weapon;
-    }
-
-    gainXp() {}
-}
-// class Mage {
-//     constructor() {
-
-//         this.spells = [];
-//     }
-
-//     gainXp() {}
-// }
-const man = new Warrior("Man", 1000, "меч");
-console.log(man);
+//  let anonym = function() {
+//      console.log("hello!")
+//  };
+let anonym = () => {
+    let count = 2;
+    return "hello!" + 2
+};
+console.log(anonym());
--- js/summBetween.js
+++ js/summBetween.js
@@ -10,3 +10,16 @@ for (let i = 5; i <= 6; i += 1) {
 console.log(sum)
 
 // done
+function getSum(a, b) {
+    let sum = 0;
+    if (a < b) {
+        for (i = a; i <= b; i += 1) {
+            sum += i;
+        }
+    } else {
+        for (i = b; i <= a; i += 1) {
+            sum += i;
+        }
+    }
+    return sum;
+}
